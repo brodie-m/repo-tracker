fdb97186f94fe5401fdeb6b45a4ce0bc693d599c
brodie-m <brodiedmcguire@gmail.com>
2021-10-20 18:00:32 +0100

    added routing and pages

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-import React from 'react'
-import logo from './logo.svg';
-import './App.css';
-
-
+import React from "react";
+import "./App.css";
+import { Welcome, Repos } from "./pages";
+import { Route, Switch } from "react-router-dom";
 function App() {
-  
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      
+    <Route path = '/' exact>
+      <Welcome/>
+    </Route>
+    <Route path = '/repos' exact>
+      <Repos/>
+    </Route>
+
+      
+    </Switch>
   );
 }
 
